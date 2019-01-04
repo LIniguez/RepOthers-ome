c602c04d39f98f4a885d286a0aca10c73612b81d
Luis Iniguez <lpr4001@med.cornell.edu>
2019-01-04 11:25:54 -0500

    Adding theta prior to Telescope

--- scripts/network_analysis.sh
+++ scripts/network_analysis.sh
@@ -31,16 +31,16 @@ size_wei=$(echo $(wc -c ${FOLDERO}/vertex_weight.txt)| cut -f1 -d ' ')
 while [ $size_wei -gt 0 ];
 do
  find_communities.R ${FOLDERO}/ ${i} ${MAX} ${WTM} 2>/dev/null
- 
+
  mod_txt2gtf.sh ${FOLDERO}/ ${i}
- ls ${FOLDERO}/*_${i}.gtf | parallel -j ${NUMPRO} -q telescope assign --outdir ${FOLDERO}/ --tempdir ${FOLDERO}/ --quiet --exp_tag ${i}round_'{= s:_WT.+::; s:^.+${FOLDERO}/::; =}' --updated_sam ${FOLDERO}/${i}.SAM {}
+ ls ${FOLDERO}/*_${i}.gtf | parallel -j ${NUMPRO} -q telescope assign --theta_prior 200000 --max_iter 200 --outdir ${FOLDERO}/ --tempdir ${FOLDERO}/ --quiet --exp_tag ${i}round_'{= s:_WT.+::; s:^.+${FOLDERO}/::; =}' --updated_sam ${FOLDERO}/${i}.SAM {}
  for j in $(find ${FOLDERO}/*_${i}.gtf)
  do
   k=$(echo $j| rev | cut -f 4 -d "_" | cut -f 1 -d "/"| rev)
   samtools sort -@ ${NUMPRO} -o ${FOLDERO}/${i}round_${k}-updated_sorted.bam ${FOLDERO}/${i}round_${k}-updated.bam
   rm ${FOLDERO}/${i}round_${k}-updated.bam ${FOLDERO}/${i}round_${k}-checkpoint.npz ${FOLDERO}/${i}round_${k}-tmp_tele.bam ${FOLDERO}/${i}round_${k}-other.bam
  done
- 
+
  merge_mod_bam_2.sh ${FOLDERO}/ ${NUMPRO} ${i} ${GEN4SMT} ${GEN4BDT} ${BED}
  i=$(($i+1))
  size_wei=$(echo $(wc -c ${FOLDERO}/vertex_weight.txt)| cut -f1 -d ' ')
@@ -53,6 +53,3 @@ samtools sort -@ ${NUMPRO} -o ${FOLDERO}/result_sorted.bam ${FOLDERO}/result.bam
 
 
 rm ${FOLDERO}/result.bam ${FOLDERO}/result.sam ${FOLDERO}/header.txt ${FOLDERO}/*uniqreads.sam ${FOLDERO}/*-telescope_report.tsv ${FOLDERO}/*.SAM ${FOLDERO}/*_commuity_*.gtf ${FOLDERO}/vertex_weight.txt ${FOLDERO}/*_telescope_res*
-
-
-
