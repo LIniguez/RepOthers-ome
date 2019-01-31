a3b21587c22677bac21a54a12fa7fc11e9388a55
Luis Iniguez <lpr4001@med.cornell.edu>
2019-01-31 12:38:03 -0500

    Variables homologed merge_mod_bam

--- scripts/scripts_sd/merge_mod_bam.sh
+++ scripts/scripts_sd/merge_mod_bam.sh
@@ -3,49 +3,43 @@
 set -e
 set -u
 
-FOLDER=$1
-NUMPROC=$2
-ROUND=$3
-GEN4SMT=$4
-GEN4BDT=$5
+FOLDOUT=$1
+NPRO=$2
+I=$3
+GEN4ST=$4
+GEN4BT=$5
 BED=$6
 
 
-samtools merge -@ ${NUMPROC} -f ${FOLDER}/${ROUND}_temp.bam ${FOLDER}/${ROUND}round_*-updated_sorted.bam
-samtools view -H ${FOLDER}/${ROUND}_temp.bam | grep -P '@SQ'| awk '{split($2,a,":"); split($3,b,":"); print a[2],b[2]}' OFS="\t" >${FOLDER}/header_mod.txt
-samtools view -@ ${NUMPROC} -L ${GEN4SMT} ${FOLDER}/${ROUND}_temp.bam | awk '{ for (i=1; i<=NF; ++i) { if ($i ~ "XP:i") {split($i,a,":");if(a[3]>=90){ if($0 !~ "ZF:Z:__no_feature"){print $0;next;}}}}}' OFS="\t" > ${FOLDER}/${ROUND}_telescope_res.sam #90 means the confiability of read assignment. 
-samtools view -@ ${NUMPROC} -t ${FOLDER}/header_mod.txt -b ${FOLDER}/${ROUND}_telescope_res.sam > ${FOLDER}/${ROUND}_2.bam  #remove sequence not present in gen4smt regions
-#samtools sort -@ ${NUMPROC} -o ${FOLDER}/${ROUND}_2.bam ${FOLDER}/${ROUND}_telescope_res.bam
+samtools merge -@ ${NPRO} -f ${FOLDOUT}/${I}_temp.bam ${FOLDOUT}/${I}round_*-updated_sorted.bam
+samtools view -H ${FOLDOUT}/${I}_temp.bam | grep -P '@SQ'| awk '{split($2,a,":"); split($3,b,":"); print a[2],b[2]}' OFS="\t" >${FOLDOUT}/header_mod.txt
+samtools view -@ ${NPRO} -L ${GEN4ST} ${FOLDOUT}/${I}_temp.bam | awk '{ for (i=1; i<=NF; ++i) { if ($i ~ "XP:i") {split($i,a,":");if(a[3]>=90){ if($0 !~ "ZF:Z:__no_feature"){print $0;next;}}}}}' OFS="\t" > ${FOLDOUT}/${I}_telescope_res.sam #90 means the confiability of read assignment.
+samtools view -@ ${NPRO} -t ${FOLDOUT}/header_mod.txt -b ${FOLDOUT}/${I}_telescope_res.sam > ${FOLDOUT}/${I}_2.bam  #remove sequence not present in gen4smt regions
 
-
-
-
-
-bedtools intersect -sorted -a ${BED} -b ${FOLDER}/${ROUND}_2.bam -wo -g ${GEN4BDT} | sort --parallel ${NUMPROC} -V -u -k 1,3 -k 8,8 > ${FOLDER}/${ROUND}_intersected_reads_4cov.bed
+bedtools intersect -sorted -a ${BED} -b ${FOLDOUT}/${I}_2.bam -wo -g ${GEN4BT} | sort --parallel ${NPRO} -V -u -k 1,3 -k 8,8 > ${FOLDOUT}/${I}_intersected_reads_4cov.bed
 
 perl -e '{open(IN,"$ARGV[0]");while(<IN>){@vec=split("\t",$_);$name=$vec[0]."_".$vec[1]."_".$vec[2]; push(@{$h{$vec[7]}}, $name);}close (IN);
  foreach $k (sort keys %h){for ($cont=0;$h{$k}[$cont];$cont++){for($cont2=$cont+1;$h{$k}[$cont2];$cont2++){$out= $h{$k}[$cont]."\t".$h{$k}[$cont2];$done{$out}++;}}} undef %h;
- foreach $k (sort keys %done){print "$k\t$done{$k}\n";} }' ${FOLDER}/${ROUND}_intersected_reads_4cov.bed > ${FOLDER}/${ROUND}_vertex_weight_multiple.txt
-cut -f 8 ${FOLDER}/${ROUND}_intersected_reads_4cov.bed | sort| uniq -c | awk '{if($1>1)print $2;}' OFS="\t" >${FOLDER}/${ROUND}_multiple_reads.txt
+ foreach $k (sort keys %done){print "$k\t$done{$k}\n";} }' ${FOLDOUT}/${I}_intersected_reads_4cov.bed > ${FOLDOUT}/${I}_vertex_weight_multiple.txt
+cut -f 8 ${FOLDOUT}/${I}_intersected_reads_4cov.bed | sort| uniq -c | awk '{if($1>1)print $2;}' OFS="\t" >${FOLDOUT}/${I}_multiple_reads.txt
 
 perl -e '{open(MU, "$ARGV[0]");while($line=<MU>){chomp $line; $mltread{$line}=1;}
  open(IN, "$ARGV[1]");while($line=<IN>){chomp $line; @vec=split("\t",$line); if(!$mltread{$vec[7]}){$name=$vec[0]."_".$vec[1]."_".$vec[2];$h{$name}++;}}close(IN);
  open(VE, "$ARGV[2]"); while($line=<VE>){
-  chomp $line; @vec=split("\t",$line); if(!$h{$vec[0]}){$h{$vec[0]}=0;}if(!$h{$vec[1]}){$h{$vec[1]}=0;} $w=$vec[2]+$h{$vec[0]}+$h{$vec[1]};print "$line\t$h{$vec[0]}\t$h{$vec[1]}\t$w\n";}}' ${FOLDER}/${ROUND}_multiple_reads.txt ${FOLDER}/${ROUND}_intersected_reads_4cov.bed ${FOLDER}/${ROUND}_vertex_weight_multiple.txt > ${FOLDER}/vertex_weight.txt
+  chomp $line; @vec=split("\t",$line); if(!$h{$vec[0]}){$h{$vec[0]}=0;}if(!$h{$vec[1]}){$h{$vec[1]}=0;} $w=$vec[2]+$h{$vec[0]}+$h{$vec[1]};print "$line\t$h{$vec[0]}\t$h{$vec[1]}\t$w\n";}}' ${FOLDOUT}/${I}_multiple_reads.txt ${FOLDOUT}/${I}_intersected_reads_4cov.bed ${FOLDOUT}/${I}_vertex_weight_multiple.txt > ${FOLDOUT}/vertex_weight.txt
 
-rm ${FOLDER}/${ROUND}round_*-updated_sorted.bam   ${FOLDER}/${ROUND}_intersected_reads_4cov.bed ${FOLDER}/${ROUND}_vertex_weight_multiple.txt ${FOLDER}/${ROUND}_multiple_reads.txt
+rm ${FOLDOUT}/${I}round_*-updated_sorted.bam   ${FOLDOUT}/${I}_intersected_reads_4cov.bed ${FOLDOUT}/${I}_vertex_weight_multiple.txt ${FOLDOUT}/${I}_multiple_reads.txt
 
-samtools view -@ ${NUMPROC} -F 256 ${FOLDER}/${ROUND}_2.bam -U ${FOLDER}/${ROUND}_flag256.sam > ${FOLDER}/${ROUND}_flag016.sam
-awk '{b=$1","$3","$4; if (!(b in a)){a[b] = $0;} } END { for (i in a) print a[i]}' ${FOLDER}/${ROUND}_flag016.sam > ${FOLDER}/${ROUND}_flag0162.sam &
-awk '{b=$1","$3","$4; if (!(b in a)){a[b] = $0;} } END { for (i in a) print a[i]}' ${FOLDER}/${ROUND}_flag256.sam > ${FOLDER}/${ROUND}_flag2562.sam &
+samtools view -@ ${NPRO} -F 256 ${FOLDOUT}/${I}_2.bam -U ${FOLDOUT}/${I}_flag256.sam > ${FOLDOUT}/${I}_flag016.sam
+awk '{b=$1","$3","$4; if (!(b in a)){a[b] = $0;} } END { for (i in a) print a[i]}' ${FOLDOUT}/${I}_flag016.sam > ${FOLDOUT}/${I}_flag0162.sam &
+awk '{b=$1","$3","$4; if (!(b in a)){a[b] = $0;} } END { for (i in a) print a[i]}' ${FOLDOUT}/${I}_flag256.sam > ${FOLDOUT}/${I}_flag2562.sam &
 wait
-#samtools view -H ${FOLDER}/${ROUND}_temp.bam > ${FOLDER}/header.txt
 
 
-cut -f 1 ${FOLDER}/${ROUND}_flag0162.sam | sort --parallel ${NUMPROC}| uniq -c | awk '{if ($1>1){print $2}}' > ${FOLDER}/${ROUND}_readcorrection.txt &
-cut -f 1 ${FOLDER}/${ROUND}_flag2562.sam | sort --parallel ${NUMPROC} -u > ${FOLDER}/${ROUND}_readnonuniq.txt &
+cut -f 1 ${FOLDOUT}/${I}_flag0162.sam | sort --parallel ${NPRO}| uniq -c | awk '{if ($1>1){print $2}}' > ${FOLDOUT}/${I}_readcorrection.txt &
+cut -f 1 ${FOLDOUT}/${I}_flag2562.sam | sort --parallel ${NPRO} -u > ${FOLDOUT}/${I}_readnonuniq.txt &
 wait
-cat ${FOLDER}/${ROUND}_readcorrection.txt >> ${FOLDER}/${ROUND}_readnonuniq.txt
+cat ${FOLDOUT}/${I}_readcorrection.txt >> ${FOLDOUT}/${I}_readnonuniq.txt
 
 
 perl -e '{ open(IN,"$ARGV[0]");while($line=<IN>){chomp $line; $h{$line}=1;$hf{$line}=1;}
@@ -55,23 +49,21 @@ perl -e '{ open(IN,"$ARGV[0]");while($line=<IN>){chomp $line; $h{$line}=1;$hf{$l
       $hf{$vec[0]}=0;
      }else{
       $vec[1]=$vec[1]+256;
-    }} 
-   $out=join("\t",@vec); print $out;}}' ${FOLDER}/${ROUND}_readcorrection.txt ${FOLDER}/${ROUND}_flag0162.sam > ${FOLDER}/${ROUND}_modified2.sam
+    }}
+   $out=join("\t",@vec); print $out;}}' ${FOLDOUT}/${I}_readcorrection.txt ${FOLDOUT}/${I}_flag0162.sam > ${FOLDOUT}/${I}_modified2.sam
 
 
 perl -e '{ open(IN,"$ARGV[0]");while($line=<IN>){chomp $line; $h{$line}=1;}
  open(IN2,"$ARGV[1]"); while(<IN2>){@vec=split("\t",$_);
    if(!$h{$vec[0]}){print $_;}
-   }}' ${FOLDER}/${ROUND}_readnonuniq.txt ${FOLDER}/${ROUND}_flag0162.sam> ${FOLDER}/${ROUND}_uniqreads.sam
+   }}' ${FOLDOUT}/${I}_readnonuniq.txt ${FOLDOUT}/${I}_flag0162.sam> ${FOLDOUT}/${I}_uniqreads.sam
 
 
-ROUND2=$(($ROUND+1))
+I2=$(($I+1))
 
-cat ${FOLDER}/${ROUND}_modified2.sam ${FOLDER}/${ROUND}_flag2562.sam | awk '{b=$1","$3","$4; if (!(b in a)){a[b] = $0;} } END { for (i in a) print a[i]}'| sort -T ${FOLDER} --parallel ${NUMPROC} -V -k1,1 -k2,2n  > ${FOLDER}/${ROUND2}.SAM
-
-
-awk '{if ($13~/XS/){print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21;}else{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20;}}' OFS="\t" ${FOLDER}/${ROUND2}.SAM > ${FOLDER}/${ROUND}_modified2.sam
-cat ${FOLDER}/header.txt ${FOLDER}/${ROUND}_modified2.sam > ${FOLDER}/${ROUND2}.SAM
-rm  ${FOLDER}/${ROUND}_temp.bam ${FOLDER}/${ROUND}_readnonuniq.txt ${FOLDER}/${ROUND}_2.bam ${FOLDER}/${ROUND}_modified2.sam ${FOLDER}/${ROUND}_readcorrection.txt ${FOLDER}/${ROUND}_flag016.sam ${FOLDER}/${ROUND}_flag256.sam ${FOLDER}/${ROUND}_flag2562.sam ${FOLDER}/${ROUND}_flag0162.sam
+cat ${FOLDOUT}/${I}_modified2.sam ${FOLDOUT}/${I}_flag2562.sam | awk '{b=$1","$3","$4; if (!(b in a)){a[b] = $0;} } END { for (i in a) print a[i]}'| sort -T ${FOLDOUT} --parallel ${NPRO} -V -k1,1 -k2,2n  > ${FOLDOUT}/${I2}.SAM
 
 
+awk '{if ($13~/XS/){print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21;}else{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20;}}' OFS="\t" ${FOLDOUT}/${I2}.SAM > ${FOLDOUT}/${I}_modified2.sam
+cat ${FOLDOUT}/header.txt ${FOLDOUT}/${I}_modified2.sam > ${FOLDOUT}/${I2}.SAM
+rm  ${FOLDOUT}/${I}_temp.bam ${FOLDOUT}/${I}_readnonuniq.txt ${FOLDOUT}/${I}_2.bam ${FOLDOUT}/${I}_modified2.sam ${FOLDOUT}/${I}_readcorrection.txt ${FOLDOUT}/${I}_flag016.sam ${FOLDOUT}/${I}_flag256.sam ${FOLDOUT}/${I}_flag2562.sam ${FOLDOUT}/${I}_flag0162.sam
