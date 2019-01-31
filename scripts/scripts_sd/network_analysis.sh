472f4181afc2831166860d618ecf9f40ea7d38fe
Luis Iniguez <lpr4001@med.cornell.edu>
2019-01-31 12:40:02 -0500

    Variable homolged in network_analysis

--- scripts/scripts_sd/network_analysis.sh
+++ scripts/scripts_sd/network_analysis.sh
@@ -3,49 +3,46 @@
 set -e
 set -u
 
-i=0
+I=0
 MAX=$1
 WTM=$2
-NUMPRO=$3
-FOLDER=$4
-FOLDERO=$5
-GEN4SMT=$6
-GEN4BDT=$7
-WHERE=$8
+NPRO=$3
+FLD5=$4
+FOLDOUT=$5
+GEN4ST=$6
+GEN4BT=$7
+SRC=$8
 SEED=$9
-BED=${FOLDER}regions_sorted_coverage_filtered.bed
+
+samtools view -h -@ ${NPRO} ${FLD5}ALL.BAM > ${FOLDOUT}${I}.SAM
+samtools view -H ${FOLDOUT}${I}.SAM >${FOLDOUT}header.txt
 
 
-
-samtools view -h -@ ${NUMPRO} ${FOLDER}ALL.BAM > ${FOLDERO}${i}.SAM
-samtools view -H ${FOLDERO}${i}.SAM >${FOLDERO}header.txt
-
-
-cp ${FOLDER}vertex_weight.txt ${FOLDERO}vertex_weight.txt
-size_wei=$(echo $(wc -c ${FOLDERO}vertex_weight.txt)| cut -f1 -d ' ')
+cp ${FLD5}vertex_weight.txt ${FOLDOUT}vertex_weight.txt
+size_wei=$(echo $(wc -c ${FOLDOUT}vertex_weight.txt)| cut -f1 -d ' ')
 
 while [ $size_wei -gt 0 ];
 do
- Rscript ${WHERE}find_communities.R ${FOLDERO} ${i} ${MAX} ${WTM} ${SEED} 2>/dev/null
+ Rscript ${SRC}/find_communities.R ${FOLDOUT} ${I} ${MAX} ${WTM} ${SEED} 2>/dev/null
 
- sh ${WHERE}mod_txt2gtf.sh ${FOLDERO} ${i}
- ls ${FOLDERO}*_${i}.gtf | parallel -j ${NUMPRO} -q telescope assign --outdir ${FOLDERO} --tempdir ${FOLDERO} --quiet --exp_tag ${i}round_'{= s:_WT.+::; s:^.+${FOLDERO}/::; =}' --updated_sam ${FOLDERO}${i}.SAM {}
- for j in $(find ${FOLDERO}*_${i}.gtf)
+ sh ${SRC}/mod_txt2gtf.sh ${FOLDOUT} ${I}
+ ls ${FOLDOUT}*_${I}.gtf | parallel -j ${NPRO} -q telescope assign --outdir ${FOLDOUT} --tempdir ${FOLDOUT} --quiet --exp_tag ${I}round_'{= s:_WT.+::; s:^.+${FOLDOUT}/::; =}' --updated_sam ${FOLDOUT}${I}.SAM {}
+ for j in $(find ${FOLDOUT}*_${I}.gtf)
  do
   k=$(echo $j| rev | cut -f 4 -d "_" | cut -f 1 -d "/"| rev)
-  samtools sort -@ ${NUMPRO} -o ${FOLDERO}${i}round_${k}-updated_sorted.bam ${FOLDERO}${i}round_${k}-updated.bam
-  rm ${FOLDERO}${i}round_${k}-updated.bam ${FOLDERO}${i}round_${k}-checkpoint.npz ${FOLDERO}${i}round_${k}-tmp_tele.bam ${FOLDERO}${i}round_${k}-other.bam
+  samtools sort -@ ${NPRO} -o ${FOLDOUT}${I}round_${k}-updated_sorted.bam ${FOLDOUT}${I}round_${k}-updated.bam
+  rm ${FOLDOUT}${I}round_${k}-updated.bam ${FOLDOUT}${I}round_${k}-checkpoint.npz ${FOLDOUT}${I}round_${k}-tmp_tele.bam ${FOLDOUT}${I}round_${k}-other.bam
  done
 
- sh ${WHERE}merge_mod_bam.sh ${FOLDERO} ${NUMPRO} ${i} ${GEN4SMT} ${GEN4BDT} ${BED}
- i=$(($i+1))
- size_wei=$(echo $(wc -c ${FOLDERO}vertex_weight.txt)| cut -f1 -d ' ')
+ sh ${SRC}/merge_mod_bam.sh ${FOLDOUT} ${NPRO} ${I} ${GEN4ST} ${GEN4BT} ${FLD5}regions_sorted_coverage_filtered.bed
+ I=$(($I+1))
+ size_wei=$(echo $(wc -c ${FOLDOUT}vertex_weight.txt)| cut -f1 -d ' ')
 done
 
-cat ${FOLDERO}*_0.gtf > ${FOLDERO}ALL.gtf
-cat ${FOLDERO}header.txt ${FOLDERO}*uniqreads.sam > ${FOLDERO}result.sam
-samtools view -@ ${NUMPRO} -b ${FOLDERO}result.sam > ${FOLDERO}result.bam
-samtools sort -@ ${NUMPRO} -o ${FOLDERO}result_sorted.bam ${FOLDERO}result.bam
+cat ${FOLDOUT}*_0.gtf > ${FOLDOUT}ALL.gtf
+cat ${FOLDOUT}header.txt ${FOLDOUT}*uniqreads.sam > ${FOLDOUT}result.sam
+samtools view -@ ${NPRO} -b ${FOLDOUT}result.sam > ${FOLDOUT}result.bam
+samtools sort -@ ${NPRO} -o ${FOLDOUT}result_sorted.bam ${FOLDOUT}result.bam
 
 
-rm ${FOLDERO}result.bam ${FOLDERO}result.sam ${FOLDERO}header.txt ${FOLDERO}*uniqreads.sam ${FOLDERO}*-telescope_report.tsv ${FOLDERO}*.SAM ${FOLDERO}*_commuity_*.gtf ${FOLDERO}vertex_weight.txt ${FOLDERO}*_telescope_res*
+rm ${FOLDOUT}result.bam ${FOLDOUT}result.sam ${FOLDOUT}header.txt ${FOLDOUT}*uniqreads.sam ${FOLDOUT}*-telescope_report.tsv ${FOLDOUT}*.SAM ${FOLDOUT}*_commuity_*.gtf ${FOLDOUT}vertex_weight.txt ${FOLDOUT}*_telescope_res*
