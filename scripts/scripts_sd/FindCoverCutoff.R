7ad86c6d58234c3a790db76ef0fbb50ef2c26f1f
Luis Iniguez <lpr4001@med.cornell.edu>
2019-01-31 10:48:34 -0500

    FindCoverCutoff.R corrected variables

--- scripts/scripts_sd/FindCoverCutoff.R
+++ scripts/scripts_sd/FindCoverCutoff.R
@@ -6,7 +6,7 @@ args <- commandArgs(trailingOnly=TRUE)
 
 check <- strsplit(args[1], "_")[[1]]
 samp <- paste(check[1:(length(check)-2)],collapse="_")
-samp_o<-paste0(name,"_CoverageCutoff.txt")
+samp_o<-paste0(samp,"_CoverageCutoff.txt")
 tds <- constructTDS(file = args[1], fragment.size = as.numeric(args[2]),unique = FALSE)
 estimateBackground(tds, fdr.cutoff = as.numeric(args[3]))
 write.table(tds@coverageCutoff,file=samp_o, sep="\t",quote=F)
