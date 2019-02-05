fa992f1450b8167229f770717d4093efb88dd84a
Luis Iniguez <lpr4001@med.cornell.edu>
2019-02-05 17:47:43 -0500

    Scripts changed to use function file

--- scripts/scripts_sd/run_bowtie2.sh
+++ scripts/scripts_sd/run_bowtie2.sh
@@ -15,22 +15,7 @@ RT=$9
 SRC=${10}
 
 
-check_sam(){
- >${1}unique.SAM; >${1}best_alscor.txt; >${1}header.txt
- awk -v FOLD="$1" '{
- if($1~/^@/) {print $0 >> FOLD"header.txt";} #Remove header sequencs
- if(($2-256)<0){ #check if the alignment is the principal
-  if($13~/XS/){  #if the alignment has XS it means it has multiple positions in Bowtie2
-   split($12,a,":");print $0 ; print $1,a[3] >> FOLD"best_alscor.txt"; #print it to multiple.SAM and retain the info of the alignement score
-  }else if($13~/ZS/){  #if the alignment has XS it means it has multiple positions in Bowtie2
-   split($12,a,":"); print $0 ; print $1,a[3] >> FOLD"best_alscor.txt"; #print it to multiple.SAM and retain the info of the alignement score
-  }else{print $0 >> FOLD"unique.SAM";}}else{ print $0 ;}}' OFS="\t"
-}
-check_mult(){
- FOLD=$1; MAX=$2
- perl -pe '{@vec=split("\t",$_);$h{$vec[0]}++;END{open(OUT, ">$OUT");
- foreach $read(keys %h){if( $h{$read} < $MAX ){ print OUT "$read\n";}}}}' -s -- -OUT=${FOLD}multreads_done.txt -MAX=${MAX}
-}
+. ${SRC}/scripts_sd/functions.sh
 
 if [ ${UNAL} != "NA" ]; then
   bowtie2 --seed ${SEED} --un-gz ${UNAL} --no-unal --score-min L,0,1.6 -p ${NPRO} -k ${K} --very-sensitive-local -x ${BI} -U ${FASQ} 2>> ${FLDG}StarDust.log | check_sam ${FOLDOUT} | check_mult ${FOLDOUT} ${K} > ${FOLDOUT}multiple.SAM
