26bcc9bb97aed275431f949ce6e67c40fb273b40
Luis Iniguez <lpr4001@med.cornell.edu>
2019-02-06 01:12:54 -0500

    Small Error on count_reads.bash solved, removed K500 from spliced reads output

--- scripts/scripts_sd/count_reads.bash
+++ scripts/scripts_sd/count_reads.bash
@@ -17,8 +17,8 @@ EXO=$9
 awk '{b="gene_id \""$4"\"; transcript_id \""$4"\"; locus \""$4"\";"; print $1,"StarDust","transcript",$2,$3,".",".",".",b;}' OFS="\t" ${FLDG}StarDust.bed > ${FLDG}StarDust.gtf
 Rscript ${SRC}/scripts_sd/count_transcripts.R ${FLDG}StarDust.bam ${FLDG}StarDust.gtf transcript gene_id ${FLDG}StarDust FALSE &>> ${FLDG}StarDust.log
 if [ "$HIS" != "NA" ]; then
- if [ -z ${FLD4}DONE_uniq_k500.BAM ] || [ -z ${FLD4}DONE_multiple_k500.BAM ] || [ -z ${FLD4}exons_sorted.BAM ];then printf "Could not proceed spliced mapped reads were not found\n" >&2 && exit 1; fi
- samtools cat -o ${FLDG}StarDust_splicing_temp.bam ${FLD4}DONE_uniq_k500.BAM ${FLD4}DONE_multiple_k500.BAM &>> ${FLDG}StarDust.log
+ if [ -z ${FLD4}DONE_uniq.BAM ] || [ -z ${FLD4}DONE_multiple.BAM ] || [ -z ${FLD4}exons_sorted.BAM ];then printf "Could not proceed spliced mapped reads were not found\n" >&2 && exit 1; fi
+ samtools cat -o ${FLDG}StarDust_splicing_temp.bam ${FLD4}DONE_uniq.BAM ${FLD4}DONE_multiple.BAM &>> ${FLDG}StarDust.log
  samtools sort -@ ${NPRO} ${FLDG}StarDust_splicing_temp.bam -o ${FLDG}StarDust_splicing.bam &>> ${FLDG}StarDust.log
  rm ${FLDG}StarDust_splicing_temp.bam
  Rscript ${SRC}/scripts_sd/count_transcripts.R ${FLDG}StarDust_splicing.bam ${FLDG}StarDust.gtf transcript gene_id ${FLDG}StarDust_splicing TRUE &>> ${FLDG}StarDust.log
